33508d3a0d29bfae55ce7ba4ba2e85f1b7bd9944
Miles De Wind <miles.dewind@mail.gvsu.edu>
2017-10-22 18:52:29 -0400

    functions

---
+++ functions.c
@@ -0,0 +1,55 @@
+#include "functions.h"
+
+/*
+typedef sturct{
+  int i;
+  char c[1024];
+  int checksum;
+} packet;
+
+typedef struct{
+  int i;
+  int key;
+  char c[1024];
+  int checksum;
+} keyPacket;
+*/
+
+int packchecksum(packet pack){
+  int count = 0;
+  int i;
+  for(i = 0; i < strlen(pack.c); i++){
+    count += (int)pack.c[i];
+  }
+  count += pack.i;
+  return count;
+}
+
+
+int keypackchecksum(keyPacket pack){
+  int count = 0;
+  int i;
+  for(i = 0; i < strlen(pack.c); i++){
+    count += (int)pack.c[i];
+  }
+  count += pack.i;
+  count += pack.key;
+  return count;
+}
+
+int checkpackchecksum(packet pack){
+  int count = packchecksum(pack);
+  if(count != pack.checksum){
+    return -1;
+  }
+  return 1;
+}
+
+
+int checkkeypackchecksum(keyPacket pack){
+  int count = keypackchecksum(pack);
+  if(count != pack.checksum){
+    return -1;
+  }
+  return 1;
+}
